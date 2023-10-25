70ea8d578fc1ebbfc50ab0ccd7405784c66861ed
Sergei Kharchikov <callmestech@gmail.com>
2023-10-25 17:33:01 +0100

    Add basic endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Path, routing::get, Router};
+
+async fn greet() -> &'static str {
+    "Hello, world!"
+}
+
+async fn greet_by_name(name: Path<String>) -> String {
+    format!("Hello, {}!", name.to_string())
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:name", get(greet_by_name));
+
+    axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
